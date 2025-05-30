c326da0b7b5ba352e73c16a9082d566f2104b2ca
mysamrung <mysamrung@gmail.com>
2025-05-31 03:01:38 +0900

    create meshlet cull data

--- Assets/Rendering/Mesh/MeshletDebugger.cs
+++ Assets/Rendering/Mesh/MeshletDebugger.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Android.Gradle;
 using UnityEngine;
+using static UnityEditor.PlayerSettings.SplashScreen;
 
 [ExecuteAlways]
 public class MeshletDebugger : MonoBehaviour
 {
-    private List<MeshletGenerator.Meshlet> meshlets;
+    private List<Meshlet> meshlets;
+    private List<CullData> cullData;
+    private List<int> vertexBuffer;
+    private List<int> triangleBuffer;
 
     private Mesh mesh;
 
@@ -14,28 +19,125 @@ public class MeshletDebugger : MonoBehaviour
 
     private void OnEnable()
     {
-        mesh = GetComponent<MeshFilter>().mesh;
-        meshlets = MeshletGenerator.GenerateMeshlets(mesh);
-    }
+        mesh = GetComponent<MeshFilter>().sharedMesh;
+        MeshletGenerator.GenerateMeshlets(mesh, out meshlets, out vertexBuffer, out triangleBuffer);
 
-    private void OnDrawGizmosSelected()
-    {
-        if(meshlets != null && preview_meshletIndex < meshlets.Count)
-        {
-            for(int i = 0; i < meshlets[preview_meshletIndex].Triangles.Count; i += 3)
-            {
-                int v0 = meshlets[preview_meshletIndex].Triangles[i + 0];
-                int v1 = meshlets[preview_meshletIndex].Triangles[i + 1];
-                int v2 = meshlets[preview_meshletIndex].Triangles[i + 2];
+        cullData = new List<CullData>();
+        for (int meshletIndex = 0; meshletIndex < meshlets.Count; meshletIndex++) {
+            cullData.Add(MeshletCullDataGenerator.ComputeCompactCullData(mesh, meshlets[meshletIndex], vertexBuffer, triangleBuffer));
+        }
 
-                Vector3 p0 = transform.TransformPoint(mesh.vertices[v0]);
-                Vector3 p1 = transform.TransformPoint(mesh.vertices[v1]);
-                Vector3 p2 = transform.TransformPoint(mesh.vertices[v2]);
-                Gizmos.color = Color.red;
-                Gizmos.DrawLine(p0, p1);
-                Gizmos.DrawLine(p1, p2);
-                Gizmos.DrawLine(p2, p0);
+        Vector3[] vertices = new Vector3[vertexBuffer.Count];
+        int[] indices = new int[triangleBuffer.Count];
+
+        for (int meshletIndex = 0; meshletIndex < meshlets.Count; meshletIndex++) {
+            var meshlet = meshlets[meshletIndex];
+            for (int i = 0; i < meshlet.triangleCount * 3; i += 3) {
+                int triBase = meshlet.triangleOffset + i;
+
+                // Indices into meshlet-local vertex list
+                int localI0 = triangleBuffer[triBase + 0];
+                int localI1 = triangleBuffer[triBase + 1];
+                int localI2 = triangleBuffer[triBase + 2];
+
+                // Convert to global vertex indices
+                int v0 = vertexBuffer[meshlet.vertexOffset + localI0];
+                int v1 = vertexBuffer[meshlet.vertexOffset + localI1];
+                int v2 = vertexBuffer[meshlet.vertexOffset + localI2];
+
+                Vector3 p0 = mesh.vertices[v0];
+                Vector3 p1 = mesh.vertices[v1];
+                Vector3 p2 = mesh.vertices[v2];
+
+                vertices[v0] = p0;
+                vertices[v1] = p1;
+                vertices[v2] = p2;
+
+                indices[triBase + 0] = v0;
+                indices[triBase + 1] = v1;
+                indices[triBase + 2] = v2;
             }
         }
+
+        Mesh newMesh = new Mesh();
+        newMesh.vertices = vertices;
+        newMesh.triangles = indices;
+        newMesh.RecalculateNormals();
+        newMesh.RecalculateBounds();
+
+        mesh = newMesh;
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (meshlets != null )
+        {
+            for (int meshletIndex = 0; meshletIndex < meshlets.Count; meshletIndex++) {
+                if (meshletIndex != preview_meshletIndex)
+                    continue;
+
+                var meshlet = meshlets[meshletIndex];
+                for (int i = 0; i < meshlet.triangleCount * 3; i += 3) {
+                    int triBase = meshlet.triangleOffset + i;
+
+                    // Indices into meshlet-local vertex list
+                    int v0 = mesh.triangles[triBase + 0];
+                    int v1 = mesh.triangles[triBase + 1];
+                    int v2 = mesh.triangles[triBase + 2];
+
+                    Vector3 p0 = transform.TransformPoint(mesh.vertices[v0]);
+                    Vector3 p1 = transform.TransformPoint(mesh.vertices[v1]);
+                    Vector3 p2 = transform.TransformPoint(mesh.vertices[v2]);
+
+                    Vector3 center = cullData[meshletIndex].boundingSphere;
+                    float radius = cullData[meshletIndex].boundingSphere.w;
+                    Vector3 forwad = new Vector3(
+                        MeshletCullDataGenerator.UnpackByteToFloat(cullData[meshletIndex].normalX),
+                        MeshletCullDataGenerator.UnpackByteToFloat(cullData[meshletIndex].normalY),
+                        MeshletCullDataGenerator.UnpackByteToFloat(cullData[meshletIndex].normalZ)
+                    );
+
+                    center = transform.TransformPoint(center);
+                    forwad = transform.TransformDirection(forwad);
+                    center += forwad * cullData[meshletIndex].apexOffset;
+                    DrawCone(16, center, radius, forwad, 0.25f);
+                    Gizmos.color = GetColorFromSeed(meshletIndex);
+                    Gizmos.DrawLine(p0, p1);
+                    Gizmos.DrawLine(p1, p2);
+                    Gizmos.DrawLine(p2, p0);
+                }
+            }
+        }
+    }
+
+    private void DrawCone(int resolution, Vector3 origin, float radius, Vector3 forward, float coneLength) {
+        // Draw circle base of the cone
+        for (int i = 0; i < resolution; i++) {
+            float angle1 = i * Mathf.PI * 2 / resolution;
+            float angle2 = (i + 1) * Mathf.PI * 2 / resolution;
+
+            Vector3 dir1 = new Vector3(Mathf.Cos(angle1), Mathf.Sin(angle1), 0);
+            Vector3 dir2 = new Vector3(Mathf.Cos(angle2), Mathf.Sin(angle2), 0);
+
+            Quaternion rotation = Quaternion.LookRotation(forward);
+            Vector3 point1 = origin + rotation * dir1 * radius + forward * coneLength;
+            Vector3 point2 = origin + rotation * dir2 * radius + forward * coneLength;
+
+            // Draw base circle line
+            Gizmos.DrawLine(point1, point2);
+
+            // Draw lines from origin to circle points
+            Gizmos.DrawLine(origin, point1);
+        }
+    }
+
+    private Color GetColorFromSeed(int seed) {
+        System.Random random = new System.Random(seed);
+
+        // Generate RGB values between 0.0 and 1.0
+        float r = (float)random.NextDouble();
+        float g = (float)random.NextDouble();
+        float b = (float)random.NextDouble();
+
+        return new Color(r, g, b);
     }
 }
--- Assets/Rendering/Mesh/MeshletGenerator.cs
+++ Assets/Rendering/Mesh/MeshletGenerator.cs
@@ -1,102 +1,115 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+public struct Meshlet {
+    public int vertexOffset;
+    public int vertexCount;
+    public int triangleOffset;
+    public int triangleCount;
+}
+public struct CullData {
+    public Vector4 boundingSphere; // xyz = center, w = radius
+    public byte normalX, normalY, normalZ, angleEncoded; // XMUBYTEN4
+    public float apexOffset;
+}
 
 public class MeshletGenerator
 {
-    public struct Meshlet
-    {
-        public List<int> Triangles;  // Triangle indices (triplets)
-        public HashSet<int> Vertices; // Unique vertex indices used by this meshlet
-    }
 
-    // Max limits per meshlet
     const int MAX_VERTICES = 64;
     const int MAX_TRIANGLES = 124;
 
-    // Generate meshlets with connected triangles
-    public static List<Meshlet> GenerateMeshlets(Mesh mesh)
-    {
+    public static void GenerateMeshlets(
+        Mesh mesh,
+        out List<Meshlet> meshlets,
+        out List<int> meshletVertexBuffer,
+        out List<int> meshletTriangleBuffer) {
+
         int[] triangles = mesh.triangles;
         Vector3[] vertices = mesh.vertices;
 
-        // Build adjacency: triangle to neighboring triangles by shared edges
         Dictionary<int, List<int>> triAdjacency = BuildTriangleAdjacency(triangles);
-
         bool[] visited = new bool[triangles.Length / 3];
-        List<Meshlet> meshlets = new List<Meshlet>();
 
-        for (int triIndex = 0; triIndex < visited.Length; triIndex++)
-        {
+        meshlets = new List<Meshlet>();
+        meshletVertexBuffer = new List<int>();
+        meshletTriangleBuffer = new List<int>();
+
+        for (int triIndex = 0; triIndex < visited.Length; triIndex++) {
             if (visited[triIndex])
                 continue;
 
-            Meshlet meshlet = new Meshlet()
-            {
-                Triangles = new List<int>(),
-                Vertices = new HashSet<int>()
-            };
+            Dictionary<int, int> localVertMap = new();  // global index Å® local index
+            List<int> vertList = new();                  // global vertex indices for this meshlet
+            List<int> triIndices = new();               // local triangle indices (triplets)
 
             Queue<int> queue = new Queue<int>();
             queue.Enqueue(triIndex);
 
-            while (queue.Count > 0)
-            {
+            while (queue.Count > 0) {
                 int currentTri = queue.Dequeue();
                 if (visited[currentTri])
                     continue;
 
-                // Check if adding this triangle exceeds limits
-                int[] triVerts = {
-                    triangles[currentTri * 3],
-                    triangles[currentTri * 3 + 1],
-                    triangles[currentTri * 3 + 2]
-                };
+                int i0 = triangles[currentTri * 3 + 0];
+                int i1 = triangles[currentTri * 3 + 1];
+                int i2 = triangles[currentTri * 3 + 2];
 
-                // Compute how many new vertices would be added if this triangle is included
-                int newVertsCount = 0;
-                foreach (var v in triVerts)
-                    if (!meshlet.Vertices.Contains(v))
-                        newVertsCount++;
+                int[] triVerts = { i0, i1, i2 };
+                int newVerts = triVerts.Count(v => !localVertMap.ContainsKey(v));
 
-                // If adding this triangle exceeds limits, skip it
-                if (meshlet.Triangles.Count / 3 + 1 > MAX_TRIANGLES ||
-                    meshlet.Vertices.Count + newVertsCount > MAX_VERTICES)
+                if (triIndices.Count / 3 + 1 > MAX_TRIANGLES || localVertMap.Count + newVerts > MAX_VERTICES)
                     continue;
 
-                // Add triangle and its vertices
-                meshlet.Triangles.AddRange(triVerts);
-                foreach (var v in triVerts)
-                    meshlet.Vertices.Add(v);
+                // Add vertices and build local remap
+                foreach (int v in triVerts) {
+                    if (!localVertMap.ContainsKey(v)) {
+                        byte localIndex = (byte)localVertMap.Count;
+                        localVertMap[v] = localIndex;
+                        vertList.Add(v); // global index list
+                    }
+                }
+
+                // Add triangle using local indices
+                triIndices.Add(localVertMap[i0]);
+                triIndices.Add(localVertMap[i1]);
+                triIndices.Add(localVertMap[i2]);
 
                 visited[currentTri] = true;
 
-                // Enqueue neighbors
-                if (triAdjacency.TryGetValue(currentTri, out var neighbors))
-                {
+                if (triAdjacency.TryGetValue(currentTri, out var neighbors)) {
                     foreach (int nbr in neighbors)
                         if (!visited[nbr])
                             queue.Enqueue(nbr);
                 }
             }
 
-            if (meshlet.Triangles.Count > 0)
-                meshlets.Add(meshlet);
-        }
+            if (triIndices.Count == 0)
+                continue;
 
-        return meshlets;
+            Meshlet m = new Meshlet {
+                vertexOffset = meshletVertexBuffer.Count,
+                vertexCount = vertList.Count,
+                triangleOffset = meshletTriangleBuffer.Count,
+                triangleCount = triIndices.Count / 3
+            };
+
+            meshlets.Add(m);
+            meshletVertexBuffer.AddRange(vertList);
+            meshletTriangleBuffer.AddRange(triIndices);
+        }
     }
 
     // Build adjacency map from triangle index to neighboring triangle indices
-    static Dictionary<int, List<int>> BuildTriangleAdjacency(int[] triangles)
-    {
+    private static Dictionary<int, List<int>> BuildTriangleAdjacency(int[] triangles) {
         Dictionary<(int, int), List<int>> edgeToTriangles = new Dictionary<(int, int), List<int>>();
 
         // Helper to create ordered edge key (min, max)
         (int, int) MakeEdge(int a, int b) => a < b ? (a, b) : (b, a);
 
         int triCount = triangles.Length / 3;
-        for (int i = 0; i < triCount; i++)
-        {
+        for (int i = 0; i < triCount; i++) {
             int i0 = triangles[i * 3];
             int i1 = triangles[i * 3 + 1];
             int i2 = triangles[i * 3 + 2];
@@ -108,10 +121,8 @@ public class MeshletGenerator
                 MakeEdge(i2, i0)
             };
 
-            foreach (var edge in edges)
-            {
-                if (!edgeToTriangles.TryGetValue(edge, out var triList))
-                {
+            foreach (var edge in edges) {
+                if (!edgeToTriangles.TryGetValue(edge, out var triList)) {
                     triList = new List<int>();
                     edgeToTriangles[edge] = triList;
                 }
@@ -124,17 +135,14 @@ public class MeshletGenerator
         for (int i = 0; i < triCount; i++)
             adjacency[i] = new List<int>();
 
-        foreach (var kvp in edgeToTriangles)
-        {
+        foreach (var kvp in edgeToTriangles) {
             var connectedTris = kvp.Value;
             if (connectedTris.Count < 2)
                 continue; // edge belongs to only one triangle
 
             // For each pair of triangles sharing this edge, add adjacency
-            for (int j = 0; j < connectedTris.Count; j++)
-            {
-                for (int k = j + 1; k < connectedTris.Count; k++)
-                {
+            for (int j = 0; j < connectedTris.Count; j++) {
+                for (int k = j + 1; k < connectedTris.Count; k++) {
                     int t0 = connectedTris[j];
                     int t1 = connectedTris[k];
                     adjacency[t0].Add(t1);
@@ -144,5 +152,85 @@ public class MeshletGenerator
         }
 
         return adjacency;
+    }
+}
+
+public class MeshletCullDataGenerator {
+
+    public static byte PackFloatToByte(float f) => (byte)Mathf.Clamp(Mathf.RoundToInt(f * 127.0f + 128.0f), 0, 255);
+    public static float UnpackByteToFloat(byte b) => (b - 128) / 127.0f;
+
+    public static CullData ComputeCompactCullData(Mesh mesh, Meshlet meshlet, List<int> vertexBuffer, List<int> triangleBuffer) {
+        Vector3[] vertices = mesh.vertices;
+
+        // Compute triangle normals
+        List<Vector3> normals = new List<Vector3>();
+        Vector3 center = Vector3.zero;
+
+        for (int i = 0; i < meshlet.triangleCount; i++) {
+            int triBase = meshlet.triangleOffset + i * 3;
+            int i0 = vertexBuffer[meshlet.vertexOffset + triangleBuffer[triBase + 0]];
+            int i1 = vertexBuffer[meshlet.vertexOffset + triangleBuffer[triBase + 1]];
+            int i2 = vertexBuffer[meshlet.vertexOffset + triangleBuffer[triBase + 2]];
+
+            Vector3 v0 = vertices[i0];
+            Vector3 v1 = vertices[i1];
+            Vector3 v2 = vertices[i2];
+
+            center += v0 + v1 + v2;
+
+            Vector3 normal = Vector3.Cross(v1 - v0, v2 - v0).normalized;
+            normals.Add(normal);
+        }
+
+        center /= (meshlet.triangleCount * 3);
+
+        // Average cone axis
+        Vector3 axis = Vector3.zero;
+        foreach (var n in normals)
+            axis += n;
+        axis.Normalize();
+
+        // Compute minimum dot product (cosine of angle to axis)
+        float minDot = 1f;
+        foreach (var n in normals)
+            minDot = Mathf.Min(minDot, Vector3.Dot(axis, n));
+
+        float coneAngle = Mathf.Acos(minDot); // radians
+        float anglePlus90 = coneAngle + Mathf.Deg2Rad * 90f;
+        float minusCos = -Mathf.Cos(anglePlus90);
+
+        // Bounding sphere
+        Vector3 sphereCenter = Vector3.zero;
+        float maxRadius = 0f;
+        for (int i = 0; i < meshlet.vertexCount; i++) {
+            int vi = vertexBuffer[meshlet.vertexOffset + i];
+            Vector3 p = vertices[vi];
+            sphereCenter += p;
+        }
+        sphereCenter /= meshlet.vertexCount;
+
+        for (int i = 0; i < meshlet.vertexCount; i++) {
+            int vi = vertexBuffer[meshlet.vertexOffset + i];
+            maxRadius = Mathf.Max(maxRadius, Vector3.Distance(sphereCenter, vertices[vi]));
+        }
+
+        // Compute apex offset = distance from sphere center to apex along cone axis
+        float apexOffset = Vector3.Dot(sphereCenter - center, axis);
+
+        // Pack cone axis as bytes (from -1..1 to 0..255)
+        byte bx = PackFloatToByte(axis.x);
+        byte by = PackFloatToByte(axis.y);
+        byte bz = PackFloatToByte(axis.z);
+        byte ba = PackFloatToByte(minusCos); // angle encoded
+
+        return new CullData {
+            boundingSphere = new Vector4(sphereCenter.x, sphereCenter.y, sphereCenter.z, maxRadius),
+            normalX = bx,
+            normalY = by,
+            normalZ = bz,
+            angleEncoded = ba,
+            apexOffset = apexOffset
+        };
     }
 }
